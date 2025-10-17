4aa93c8a2e2fa7c438d5c1f7c0aa88ab88d4f650
talizu <talissonsousa.social@gmail.com>
2025-10-17 23:21:46 +0000

    02:30min | Atividade Rélógio

--- poo/relogio/py/draft.py
+++ poo/relogio/py/draft.py
@@ -4,77 +4,110 @@ class Relogio:
         self.__min = 0
         self.__seg = 0
 
-    def get_hora(self)-> int:
+    def get_hora(self) -> int:
         return self.__hora
-    def get_min(self)-> int:
+
+    def get_min(self) -> int:
         return self.__min
-    def get_seg(self)-> int:
+
+    def get_seg(self) -> int:
         return self.__seg
-    
-    def nextSecond (self):
-        self.__seg += 1
-        if self.__seg > 59:
-            self.__seg = 0
-            self.__min += 1
-                
-    def nextSecond (self):
+
+    def set_hora(self, valor: int):
+        if 0 <= valor < 24:
+            self.__hora = valor
+        else:
+            print("fail: hora invalida")
+
+    def set_min(self, valor: int):
+        if 0 <= valor < 60:
+            self.__min = valor
+        else:
+            print("fail: minuto invalido")
+
+    def set_seg(self, valor: int):
+        if 0 <= valor < 60:
+            self.__seg = valor
+        else:
+            print("fail: segundo invalido")
+
+    def init(self, h: int, m: int, s: int):
+        self.__hora = 0
+        self.__min = 0
+        self.__seg = 0
+        if not (0 <= h < 24):
+            print("fail: hora invalida")
+        else:
+            self.__hora = h
+        if not (0 <= m < 60):
+            print("fail: minuto invalido")
+        else:
+            self.__min = m
+        if not (0 <= s < 60):
+            print("fail: segundo invalido")
+        else:
+            self.__seg = s
+
+    def nextSecond(self):
         self.__seg += 1
         if self.__seg > 59:
             self.__seg = 0
             self.__min += 1
         if self.__min > 59:
             self.__min = 0
-            if self.__hora <= 23:
-                self.__hora += 1
-            if self.__hora > 23:
-                self.__hora = 0
+            self.__hora += 1
+        if self.__hora > 23:
+            self.__hora = 0
 
-    def set_hora(self, valor: int):
-        if valor > 23:
-            print("fail: hora invalida")
-        elif valor >=0 and valor <=23: 
-            self.__hora = valor
-    def set_min(self, valor: int):
-        if valor > 59:
-            print("fail: minuto invalido")
-        elif valor >= 0 and valor <=59: 
-            self.__min = valor
-    def set_seg(self, valor: int):
-        if valor > 59:
-            print("fail: segundo invalido")
-        elif valor >= 0 and valor <=59: 
-            self.__seg = valor
-    
-    def __str__ (self) -> str:
+    def __str__(self) -> str:
         return f"{self.__hora:02}:{self.__min:02}:{self.__seg:02}"
-    
+
+
 def main():
     time = Relogio()
-    while True: #loop infinito 
-        line: str = input()
-        print ("$"+line)
-        args: list[str]=line.split(" ")
-
-        if args[0] == "end":
+    while True:
+        try:
+            line: str = input()
+        except EOFError:
             break
-        elif args[0]=="show":
+        if not line.strip():
+            continue
+        print("$" + line)
+        args = line.split()
+        cmd = args[0]
+        if cmd == "end":
+            break
+        elif cmd == "show":
             print(time)
-
-        elif args[0]=="set":
-            if len(args) == 4: 
+        elif cmd == "set":
+            if len(args) != 4:
+                print("fail: use 'set hora min seg'")
+                continue
+            try:
                 hora = int(args[1])
                 minuto = int(args[2])
                 seg = int(args[3])
                 time.set_hora(hora)
                 time.set_min(minuto)
                 time.set_seg(seg)
-
-        elif args[0] =="next":
+            except ValueError:
+                print("fail: argumentos devem ser inteiros")
+        elif cmd == "init":
+            if len(args) != 4:
+                print("fail: use 'init hora min seg'")
+                continue
+            try:
+                h = int(args[1])
+                m = int(args[2])
+                s = int(args[3])
+                time.init(h, m, s)
+            except ValueError:
+                print("fail: argumentos devem ser inteiros")
+        elif cmd == "next":
             time.nextSecond()
-
         else:
             print("fail: comando invalido")
 
-            
+
 if __name__ == "__main__":
     main()
